be46d762ba14d41639cbbd9a915891bde36c252d
rita <s2rita0222@gmail.com>
2020-05-07 18:10:52 +0900

    記事公開第1弾

--- sample/WindowsFormsApp1/Form1.cs
+++ sample/WindowsFormsApp1/Form1.cs
@@ -9,9 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 
-using SharpDX.DirectInput;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Reactive.Disposables;
 using System.Diagnostics;
 using System.Reactive;
@@ -27,77 +25,38 @@ namespace WindowsFormsApp1
         }
     }
 
-    struct KeyInfo
-    {
-        public char Key;
-        public bool State;
-        public int Frame;
-        public override string ToString() => Key.ToString();
-    }
-
-    /// 
-    /// オリジナルのウィンドウフォームを生成する為のクラス
-    /// 
     public partial class GameForm : Form, IDisposable
     {
-        /// 
-        /// ゲームパッド取得用変数
-        /// 
-        private Joystick _joy;
+        private readonly InputManager input = new InputManager();
 
-        private JoystickState _prevState = new JoystickState();
-
-        private Subject<KeyInfo> _direction = new Subject<KeyInfo>();
-
-        private Subject<KeyInfo>[] _buttons = new Subject<KeyInfo>[4];
-
-        private CompositeDisposable _cd = new CompositeDisposable();
-
-        private int frame = 0;
+        private readonly CompositeDisposable _cd = new CompositeDisposable();
 
         private IObservable<KeyInfo> CreateCommandObserver(string command)
         {
-            IObservable<KeyInfo> inputObserverSelector(char c)
+            IObservable<KeyInfo> getInputObserver(char c)
             {
-                IObservable<KeyInfo> result = null;
-                switch (c)
-                {
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                        result = _direction;
-                        break;
-                    case 'A':
-                        result = _buttons[0];
-                        break;
-                    case 'B':
-                        result = _buttons[1];
-                        break;
-                    case 'C':
-                        result = _buttons[2];
-                        break;
-                    case 'D':
-                        result = _buttons[3];
-                        break;
-                }
-
-                return result.Where(d => d.Key == c && d.State);
+                return input.KeyStream.Where(k => k.Key == c && k.State);
             }
 
-            var observer = inputObserverSelector(command[0]);
+            bool isDirection(char c)
+            {
+                return '1' <= c && c <= '9';
+            }
+
+            var observer = getInputObserver(command[0]);
             for (int i = 1; i < command.Length; ++i)
             {
                 var index = i;
+                if (command[index] != command[index - 1])
+                {
+                    observer = observer
+                        .Merge(getInputObserver(command[index]));
+                }
+
                 observer = observer
-                    .Merge(inputObserverSelector(command[index]))
                     .Buffer(2, 1)
-                    .Where(b => i == 1 ? true : b[1].Frame - b[0].Frame < 16)
+                    .Where(b => (index == 1 && isDirection(command[0]))
+                             || b[1].Frame - b[0].Frame < 16)
                     .Where(b => b[0].Key == command[index - 1]
                              && b[1].Key == command[index])
                     .Select(b => b[1]);
@@ -120,11 +79,6 @@ namespace WindowsFormsApp1
 
             // 最大化を無効にする
             MaximizeBox = false;
-
-            for (int i = 0; i < 4; ++i)
-            {
-                _buttons[i] = new Subject<KeyInfo>();
-            }
 
             var hadohken = CreateCommandObserver("236A")
                 .Do(_ => Debug.WriteLine("波動拳！"))
@@ -155,6 +109,22 @@ namespace WindowsFormsApp1
             var shungoku = CreateCommandObserver("AA6BC")
                 .Do(_ => Debug.WriteLine("一瞬千撃！"))
                 .Subscribe().AddTo(_cd);
+
+            var hyakuretu = this.input.KeyStream
+                .Where(k => k.Key == 'D' && k.State)
+                .Buffer(4, 1)
+                .Where(b => b[3].Frame - b[0].Frame < 30)
+                .Do(_ => Debug.WriteLine("やっやっやっ"))
+                .Subscribe().AddTo(_cd);
+
+            var hatsudo = this.input.KeyStream
+                .Where(k => (k.Key == 'A' || k.Key == 'B' || k.Key == 'C') && k.State)
+                .Buffer(3, 1)
+                .Where(b => b.Max(k => k.Frame) - b.Min(k => k.Frame) < 2)
+                .Select(b => b.Select(k => k.Key))
+                .Where(a => a.Contains('A') && a.Contains('B') && a.Contains('C'))
+                .Do(_ => Debug.WriteLine("じゃきーん！"))
+                .Subscribe().AddTo(_cd);
         }
 
         /// 
@@ -162,8 +132,6 @@ namespace WindowsFormsApp1
         /// 
         public void Exec()
         {
-            Initialize();
-
             // フォームの生成
             Show();
             // フォームが作成されている間は、ループし続ける
@@ -181,79 +149,11 @@ namespace WindowsFormsApp1
         }
 
         /// 
-        /// DirectXデバイスの初期化
-        /// 
-        public void Initialize()
-        {
-            // 入力周りの初期化
-            DirectInput dinput = new DirectInput();
-            if (dinput != null)
-            {
-                // 使用するゲームパッドのID
-                var joystickGuid = Guid.Empty;
-                // ゲームパッドからゲームパッドを取得する
-                if (joystickGuid == Guid.Empty)
-                {
-                    foreach (DeviceInstance device in dinput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
-                    {
-                        joystickGuid = device.InstanceGuid;
-                        break;
-                    }
-                }
-                // ジョイスティックからゲームパッドを取得する
-                if (joystickGuid == Guid.Empty)
-                {
-                    foreach (DeviceInstance device in dinput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
-                    {
-                        joystickGuid = device.InstanceGuid;
-                        break;
-                    }
-                }
-                // 見つかった場合
-                if (joystickGuid != Guid.Empty)
-                {
-                    // パッド入力周りの初期化
-                    _joy = new Joystick(dinput, joystickGuid);
-                    if (_joy != null)
-                    {
-                        // バッファサイズを指定
-                        _joy.Properties.BufferSize = 128;
-
-                        // 相対軸・絶対軸の最小値と最大値を
-                        // 指定した値の範囲に設定する
-                        foreach (DeviceObjectInstance deviceObject in _joy.GetObjects())
-                        {
-                            switch (deviceObject.ObjectId.Flags)
-                            {
-                                case DeviceObjectTypeFlags.Axis:
-                                // 絶対軸or相対軸
-                                case DeviceObjectTypeFlags.AbsoluteAxis:
-                                // 絶対軸
-                                case DeviceObjectTypeFlags.RelativeAxis:
-                                    // 相対軸
-                                    var ir = _joy.GetObjectPropertiesById(deviceObject.ObjectId);
-                                    if (ir != null)
-                                    {
-                                        try
-                                        {
-                                            ir.Range = new InputRange(-1000, 1000);
-                                        }
-                                        catch (Exception) { }
-                                    }
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        /// 
         /// メインループ処理
         /// 
         public void MainLoop()
         {
-            UpdateForPad();
+            this.input.Update();
         }
 
         /// 
@@ -262,95 +162,6 @@ namespace WindowsFormsApp1
         public new void Dispose()
         {
             base.Dispose();
-        }
-
-        /// 
-        /// パッド入力処理
-        /// 
-        public void UpdateForPad()
-        {
-            // フォームにフォーカスが無い場合、処理終了
-            if (!Focused) { return; }
-            // 初期化が出来ていない場合、処理終了
-            if (_joy == null) { return; }
-
-            // キャプチャするデバイスを取得
-            _joy.Acquire();
-            _joy.Poll();
-
-            // ゲームパッドのデータ取得
-            var jState = _joy.GetCurrentState();
-            // 取得できない場合、処理終了
-            if (jState == null) { return; }
-
-            if (jState.X != _prevState.X || jState.Y != _prevState.Y)
-            {
-                var value = 5;
-                if (jState.X > 300) value += 1;
-                if (jState.X < -300) value -= 1;
-                if (jState.Y < -300) value += 3;
-                if (jState.Y > 300) value -= 3;
-                _direction.OnNext(new KeyInfo
-                {
-                    Key = value.ToString()[0],
-                    State = true,
-                    Frame = frame,
-                });
-            }
-
-            var buttonName = new[] { 'A', 'B', 'C', 'D' };
-            for (int i = 0; i < 4; ++i)
-            {
-                if (jState.Buttons[i] != _prevState.Buttons[i])
-                {
-                    _buttons[i].OnNext(new KeyInfo
-                    {
-                        Key = buttonName[i],
-                        State = jState.Buttons[i],
-                        Frame = frame,
-                    });
-                }
-            }
-
-            _prevState = jState;
-            ++frame;
-
-            // 以下の処理は挙動確認用
-
-            // 挙動確認用：押されたキーをタイトルバーに表示する
-            // アナログスティックの左右軸
-            bool inputX = true;
-            if (jState.X > 300)
-            {
-                Text = "入力キー：→";
-            }
-            else if (jState.X < -300)
-            {
-                Text = "入力キー：←";
-            }
-            else
-            {
-                inputX = false;
-            }
-            // アナログスティックの上下軸
-            bool inputY = true;
-            if (jState.Y > 300)
-            {
-                Text = "入力キー：↓";
-            }
-            else if (jState.Y < -300)
-            {
-                Text = "入力キー：↑";
-            }
-            else
-            {
-                inputY = false;
-            }
-            // 未入力だった場合
-            if (!inputX && !inputY)
-            {
-                Text = "入力キー：";
-            }
         }
     }
 }
---
+++ sample/WindowsFormsApp1/InputManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Subjects;
+using SharpDX.DirectInput;
+using System.Reactive.Linq;
+
+namespace WindowsFormsApp1
+{
+    public struct KeyInfo
+    {
+        public char Key;
+        public bool State;
+        public int Frame;
+    }
+
+    public class InputManager
+    {
+        private Joystick pad;
+        private JoystickState prevState = new JoystickState();
+        private int frame = 0;
+
+        public InputManager()
+        {
+            DirectInput dinput = new DirectInput();
+
+            // 使用するゲームパッドのID
+            var joystickGuid = Guid.Empty;
+
+            // ゲームパッドからゲームパッドを取得する
+            if (joystickGuid == Guid.Empty)
+            {
+                foreach (DeviceInstance device in dinput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                {
+                    joystickGuid = device.InstanceGuid;
+                    break;
+                }
+            }
+
+            // ジョイスティックからゲームパッドを取得する
+            if (joystickGuid == Guid.Empty)
+            {
+                foreach (DeviceInstance device in dinput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                {
+                    joystickGuid = device.InstanceGuid;
+                    break;
+                }
+            }
+
+            // 見つからなかった場合
+            if (joystickGuid == Guid.Empty)
+            {
+                return;
+            }
+
+            // パッド入力周りの初期化
+            pad = new Joystick(dinput, joystickGuid);
+            // バッファサイズを指定
+            pad.Properties.BufferSize = 128;
+
+            // 相対軸・絶対軸の最小値と最大値を
+            // 指定した値の範囲に設定する
+            foreach (DeviceObjectInstance deviceObject in pad.GetObjects())
+            {
+                switch (deviceObject.ObjectId.Flags)
+                {
+                    case DeviceObjectTypeFlags.Axis:
+                    // 絶対軸or相対軸
+                    case DeviceObjectTypeFlags.AbsoluteAxis:
+                    // 絶対軸
+                    case DeviceObjectTypeFlags.RelativeAxis:
+                        // 相対軸
+                        var ir = pad.GetObjectPropertiesById(deviceObject.ObjectId);
+                        if (ir != null)
+                        {
+                            try
+                            {
+                                ir.Range = new InputRange(-1000, 1000);
+                            }
+                            catch (Exception) { }
+                        }
+                        break;
+                }
+            }
+        }
+
+        private readonly Subject<KeyInfo> _keyStream = new Subject<KeyInfo>();
+        public IObservable<KeyInfo> KeyStream => this._keyStream.AsObservable();
+
+        public void Update()
+        {
+            if (this.pad == null) return;
+
+            this.pad.Acquire();
+            this.pad.Poll();
+
+            var state = this.pad.GetCurrentState();
+            if (state == null) return;
+
+            if (state.X != prevState.X || state.Y != prevState.Y)
+            {
+                var value = 5;
+                if (state.X > 300) value += 1;
+                if (state.X < -300) value -= 1;
+                if (state.Y < -300) value += 3;
+                if (state.Y > 300) value -= 3;
+                this._keyStream.OnNext(new KeyInfo
+                {
+                    Key = value.ToString()[0],
+                    State = true,
+                    Frame = frame,
+                });
+            }
+
+            char[] buttonName = { 'A', 'B', 'C', 'D' };
+            for (int i = 0; i < 4; ++i)
+            {
+                if (state.Buttons[i] != this.prevState.Buttons[i])
+                {
+                    this._keyStream.OnNext(new KeyInfo
+                    {
+                        Key = buttonName[i],
+                        State = state.Buttons[i],
+                        Frame = this.frame,
+                    });
+                }
+            }
+
+            this.prevState = state;
+            ++this.frame;
+        }
+    }
+}
